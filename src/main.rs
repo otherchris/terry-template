1d092e460cb8329bb1c7613a6fb71f1be26f1c8f
otherchris <chriscaragianis@gmail.com>
2023-09-29 09:13:27 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@
 #![no_std]
 #![no_main]
 use core::cell::RefCell;
-use core::f32;
 use cortex_m::singleton;
 use critical_section::Mutex;
 use defmt::*;
 use defmt_rtt as _;
-use fugit::{MicrosDurationU32, RateExtU32};
+use fugit::RateExtU32;
 use mcp4725::*;
 use panic_probe as _;
 use rp2040_hal::{
@@ -18,24 +17,16 @@ use rp2040_hal::{
     gpio::bank0::Gpio17,
     gpio::bank0::Gpio8,
     gpio::bank0::Gpio9,
-    gpio::Function,
-    gpio::FunctionUart,
-    gpio::Pin,
     gpio::Pins,
-    gpio::PullDown,
-    i2c::Controller,
     pac,
     pac::interrupt,
-    pac::UART1,
-    timer::Alarm,
-    timer::Timer,
-    uart::{self, DataBits, Enabled, StopBits, UartConfig, UartPeripheral},
+    uart::{DataBits, StopBits, UartConfig, UartPeripheral},
     watchdog::Watchdog,
     Clock, Sio, I2C,
 };
 use rp_pico::entry;
 mod types;
-use types::I2CType;
+use types::{I2CType, UartType};
 
 type DACAlarm = rp2040_hal::timer::Alarm0;
 static mut DAC_ALARM: Mutex<RefCell<Option<DACAlarm>>> = Mutex::new(RefCell::new(None));
@@ -43,26 +34,12 @@ static mut DAC_ALARM: Mutex<RefCell<Option<DACAlarm>>> = Mutex::new(RefCell::new
 type DACType = MCP4725<I2CType>;
 static mut DAC: Mutex<RefCell<Option<DACType>>> = Mutex::new(RefCell::new(None));
 
-static mut UART1_INST: Mutex<
-    RefCell<
-        Option<
-            UartPeripheral<
-                Enabled,
-                UART1,
-                (
-                    Pin<Gpio8, FunctionUart, PullDown>,
-                    Pin<Gpio9, FunctionUart, PullDown>,
-                ),
-            >,
-        >,
-    >,
-> = Mutex::new(RefCell::new(None));
+static mut UART1_INST: Mutex<RefCell<Option<UartType>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
     info!("Program start");
     let pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
     let mut resets = pac.RESETS;
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
@@ -85,7 +62,6 @@ fn main() -> ! {
     }
 
     let pins = Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut resets);
-    let mut timer = Timer::new(pac.TIMER, &mut resets, &clocks);
 
     // I2C DAC
     let dac_scl = pins.gpio17.into_function();
@@ -122,12 +98,12 @@ fn main() -> ! {
 #[interrupt]
 fn TIMER_IRQ_0() {
     critical_section::with(|cs| {
-        // let dac = unsafe { DAC.borrow(cs).take().unwrap() };
-        // let mut dac_alarm = unsafe { DAC_ALARM.borrow(cs).take().unwrap() };
+        let dac = unsafe { DAC.borrow(cs).take().unwrap() };
+        let dac_alarm = unsafe { DAC_ALARM.borrow(cs).take().unwrap() };
         // dac_alarm.clear_interrupt();
 
-        // unsafe { DAC_ALARM.borrow(cs).replace(Some(dac_alarm)) };
-        // unsafe { DAC.borrow(cs).replace(Some(dac)) };
+        unsafe { DAC_ALARM.borrow(cs).replace(Some(dac_alarm)) };
+        unsafe { DAC.borrow(cs).replace(Some(dac)) };
     });
 }
 
@@ -138,9 +114,9 @@ fn UART1_IRQ() {
         let uart = unsafe { UART1_INST.borrow(cs).take().unwrap() };
         info!("here go");
         info!("{}", uart.uart_is_readable());
-        let booger = singleton!(: [u8; 5] = [0; 5]).unwrap();
-        uart.read_full_blocking(booger).unwrap();
-        info!("{}", booger);
+        let buf = singleton!(: [u8; 5] = [0; 5]).unwrap();
+        uart.read_full_blocking(buf).unwrap();
+        info!("{}", buf);
 
         unsafe { UART1_INST.borrow(cs).replace(Some(uart)) };
     })
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,14 @@
-use crate::{pac::I2C0, Gpio16, Gpio17, Pin, I2C};
-use rp2040_hal::gpio::{FunctionI2c, PullDown};
+use crate::{pac::I2C0, Gpio16, Gpio17, Gpio8, Gpio9, I2C};
+use rp2040_hal::{
+    gpio::{FunctionI2c, FunctionUart, PullDown},
+    pac::UART1,
+    uart::{Enabled, UartPeripheral},
+};
 
 pub type SdaPin = rp2040_hal::gpio::Pin<Gpio16, FunctionI2c, PullDown>;
 pub type SclPin = rp2040_hal::gpio::Pin<Gpio17, FunctionI2c, PullDown>;
 pub type I2CType = I2C<I2C0, (SdaPin, SclPin)>;
+
+pub type TxPin = rp2040_hal::gpio::Pin<Gpio8, FunctionUart, PullDown>;
+pub type RxPin = rp2040_hal::gpio::Pin<Gpio9, FunctionUart, PullDown>;
+pub type UartType = UartPeripheral<Enabled, UART1, (TxPin, RxPin)>;
