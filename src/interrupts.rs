0bf8386c76606bacbc1d9112bf3cc8fd53e4a18d
otherchris <chriscaragianis@gmail.com>
2023-09-30 13:58:49 -0400

    dac and rotary encoder

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,13 +1,14 @@
+use rotary_encoder_embedded::Direction;
 use rp2040_hal::timer::Alarm;
 
 use crate::{
     info, interrupt, singleton, ALARM_0, ALARM_0_DURATION, ALARM_1, ALARM_1_DURATION, ALARM_2,
-    ALARM_2_DURATION, ALARM_3, ALARM_3_DURATION, UART1_INST,
+    ALARM_2_DURATION, ENCODER_1, ENCODER_POLL_ALARM, ENCODER_POLL_DURATION, UART1_INST,
 };
 
 #[interrupt]
 fn TIMER_IRQ_0() {
-    info!("alarm 0 fired and caught");
+    // info!("alarm 0 fired and caught");
     critical_section::with(|cs| {
         let alarm_0_duration = unsafe { ALARM_0_DURATION.borrow(cs).take().unwrap() };
         let mut alarm_0 = unsafe { ALARM_0.borrow(cs).take().unwrap() };
@@ -45,12 +46,31 @@ fn TIMER_IRQ_2() {
 #[interrupt]
 fn TIMER_IRQ_3() {
     critical_section::with(|cs| {
-        let alarm_3_duration = unsafe { ALARM_3_DURATION.borrow(cs).take().unwrap() };
-        let mut alarm_3 = unsafe { ALARM_3.borrow(cs).take().unwrap() };
-        alarm_3.clear_interrupt();
-        alarm_3.schedule(alarm_3_duration).ok();
-        unsafe { ALARM_3.borrow(cs).replace(Some(alarm_3)) };
-        unsafe { ALARM_3_DURATION.borrow(cs).replace(Some(alarm_3_duration)) };
+        let encoder_alarm_duration = unsafe { ENCODER_POLL_DURATION.borrow(cs).take().unwrap() };
+        let mut encoder_alarm = unsafe { ENCODER_POLL_ALARM.borrow(cs).take().unwrap() };
+        let mut encoder_1 = unsafe { ENCODER_1.borrow(cs).take().unwrap() };
+        encoder_alarm.clear_interrupt();
+        encoder_alarm.schedule(encoder_alarm_duration).ok();
+
+        encoder_1.update();
+        match encoder_1.direction() {
+            Direction::Clockwise => {
+                info!("Clockwise")
+            }
+            Direction::Anticlockwise => {
+                info!("AntiClockwise")
+            }
+            Direction::None => {
+                // info!("None")
+            }
+        }
+        unsafe { ENCODER_POLL_ALARM.borrow(cs).replace(Some(encoder_alarm)) };
+        unsafe {
+            ENCODER_POLL_DURATION
+                .borrow(cs)
+                .replace(Some(encoder_alarm_duration))
+        };
+        unsafe { ENCODER_1.borrow(cs).replace(Some(encoder_1)) };
     });
 }
 //Handle UART data
